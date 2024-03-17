85268fe27f71dc1c6dffd34fc329e1b6a06a189c
Kashif bacha <muhammadkashif3779@gmail.com>
2024-03-17 21:53:18 +0500

    updated

---
+++ lib/Resourses/images/image_assets.dart
@@ -0,0 +1,16 @@
+class ImageAssets{
+  static const nightRain="assets/images/nightRain.png";
+  static const starts="assets/images/start.png";
+  static const map="assets/images/map.png";
+  static const sunHeavyRain="assets/images/sunHeavyRain.png";
+  static const nightStarRain="assets/images/nightStatRain.png";
+  static const umbrella="assets/images/umbrella.png";
+  static const heavyRain="assets/images/heavyRain.png";
+  static const winds="assets/images/wind.png";
+  static const sun="assets/images/sun.png";
+  static const windWave="assets/images/windwave.png";
+  static const starClear="assets/images/starCLear.png";
+  static const thunder="assets/images/thunder.png";
+  static const moon="assets/images/moon.png";
+
+}
---
+++ lib/view/splash/splash_view.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/Resourses/images/image_assets.dart';
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(ImageAssets.nightStarRain,height: 200,width: 200,),
+      ),
+    );
+  }
+}
